a0ec2c1ad53686036c4a93633abf0f8fe4396f96
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2025-06-13 12:12:57 +0530

    refactor: nix-shell for rust compatibility

--- home-manager/.config/home-manager/shells/rust-shell.nix
+++ home-manager/.config/home-manager/shells/rust-shell.nix
@@ -10,13 +10,20 @@ pkgs.mkShell {
     clang
     gnumake
     just
+    llvmPackages.libclang
+    mysql84
   ];
 
   shellHook = ''
     export CARGO_HOME=$HOME/.cargo
     export RUSTUP_HOME=$HOME/.rustup
 
-    # Rust install check
+    export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib
+    export LD_LIBRARY_PATH=$LIBCLANG_PATH:$LD_LIBRARY_PATH
+
+    # Point bindgen to system headers explicitly
+    export BINDGEN_EXTRA_CLANG_ARGS="-isystem ${pkgs.glibc.dev}/include -isystem ${pkgs.glibc.dev}/lib/gcc/*/*/include -isystem ${pkgs.glibc.dev}/lib/gcc/*/*/include-fixed -isystem ${pkgs.glibc.dev}/include-fixed"
+
     if ! command -v rustc >/dev/null; then
       echo "[rust-shell] Installing rust toolchain..."
       rustup install stable
@@ -24,11 +31,10 @@ pkgs.mkShell {
       rustup component add clippy rustfmt
     fi
 
-    # Aliases inside shell only
     alias cc='cargo check'
     alias cr='cargo run'
     alias c='clear && printf "\033c"'
 
-    echo "[rust-shell] Aliases available: 'cr' = cargo run 'cc' = cargo check, 'c' = clear screen"
+    echo "[rust-shell] Aliases available: 'cr' = cargo run, 'cc' = cargo check, 'c' = clear screen"
   '';
 }
